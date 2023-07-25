cd927b03ea3c08776c8a18600091e05cb553541c
ikram12Ou <ikram345ikram@gmail.com>
2023-07-25 07:30:17 -0700

    tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,12 @@ int a, b, c, d;
 for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
-if (a >= 2 && b >= 4)
-break;
 -putchar(a + 48);
 _putchar(b + 48);
 _putchar(58);
