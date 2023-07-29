fb622235e1b09680931922506074f044c6afdeab
ikram12Ou <ikram345ikram@gmail.com>
2023-07-29 10:42:32 -0700

    tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,15 @@
  */
 void print_number(int n)
 {
-unsigned int n1;
-n1 = n;
+unsigned int i = n;
 if (n < 0)
 {
-_putchar('-');
-n1 = -n;
+_putchar(45);
+i = -i;
 }
-if (n1 / 10 != 0)
+if (i / 10)
 {
-print_number(n1 / 10);
+print_number(i / 10);
 }
-_putchar((n1 % 10) + '0');
+_putchar(i % 10 + '0');
 }
