5110849161f53e9153aae27bb177c1e5775abf0f
ikram12Ou <ikram345ikram@gmail.com>
2023-07-29 21:48:24 -0700

    tasks

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,40 @@
-#include "main.h"
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-/**
- * add_strings - add the numbers stored in two strings
- * @n1: The first number to be added
- * @n2: The second number to be added
- * @r: The buffer to store the result
- * @r_index: The current index of the buffer
- * Return: A pointer to the result or 0
- */
-char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-int num, tens = 0;
-for (; *n1 && *n2; n1--, n2 --, r_index--)
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
 {
-num = (*n1 - '0') + (*n2 - '0');
-num += tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+if (i < 0)
+val1 = 0;
+else
+val1 = *(n1 + i) -'0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2 + j) -'0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
 }
-for (; *n1; n1--; r_index++)
-{
-num = *(n1 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
-for (; *n2; n2--; r_index--)
-{
-num = (*n2 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
 }
