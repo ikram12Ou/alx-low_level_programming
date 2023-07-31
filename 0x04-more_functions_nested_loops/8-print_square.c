871c6be4afa849d36b7335f957b0451e420382c1
ikram12Ou <ikram345ikram@gmail.com>
2023-07-30 20:10:17 -0700

    tasks

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,9 @@
 void print_square(int size)
 {
 int x, y;
-if (size <= 0)
+if (size > 0)
 {
-_putchar('\n');
-}
-else
-{
-for (x = 0; x <= size; x++)
+for (x = 0; x < size; x++)
 {
 for (y = 0; y < size; y++)
 {
@@ -21,5 +17,9 @@ _putchar(35);
 }
 _putchar('\n');
 }
+}
+else
+{
+_putchar('\n');
 }
 }
