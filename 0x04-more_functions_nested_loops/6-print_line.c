4489bbf7234a63886f1e6548b7b0d4a2a1b41015
ikram12Ou <ikram345ikram@gmail.com>
2023-07-30 19:24:51 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: the number of lines
+ * Return: empty
+ */
+void print_line(int n)
+{
+int a;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < n; a++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
