20dc8f2cbf0d1c0d04f9e602cfd035aeac8fd068
ikram12Ou <ikram345ikram@gmail.com>
2023-07-30 19:32:48 -0700

    tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,17 @@
  */
 void more_numbers(void)
 {
-int a, b;
-for (a = 0; a < 10; a++)
+int n1, n2;
+for (n1 = 0; n1 < 10; n1++)
 {
-for (b = 0; y <= 14; b++)
+for (n2 = 0; n2 <= 14; n2++)
 {
-if (b > 9)
+if (n2 > 9)
 {
-_putchar((b / 10) + '0');
+_putchar((n2 / 10) + '0');
 }
-_putchar((b % 10) + '0');
+_putchar((n2 % 10) + '0');
 }
-_putchar('\n');
+_putchar(10);
 }
 }
