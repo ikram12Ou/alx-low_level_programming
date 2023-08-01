af27a2ad5dc9cb70eb5cf60a8d789c31f2b48110
ikram12Ou <ikram345ikram@gmail.com>
2023-07-31 20:22:38 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int j = 0;
+int i;
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 == 1)
+{
+i = (j - 1) / 2;
+i += 1;
+}
+else
+{
+i = j / 2;
+}
+for (; i < j; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
