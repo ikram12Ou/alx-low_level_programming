9bf5b802f02bbbf68f67ee5523f2228173713111
ikram12Ou <ikram345ikram@gmail.com>
2023-08-17 06:55:40 -0700

    tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,4 +6,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
+int main(int _attribute_((_unused)) argc, char *argv[]);
 #endif
